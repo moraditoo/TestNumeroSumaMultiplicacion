beefce6188cd7dbb68035345cf9705a75d3b5b02
Mauro Delgado <maurods@uamv.edu.ni>
2026-06-17 14:24:51 -0600

    agregar clase TestNumerico, chore: implementar logica de Clase TestNumerico

---
+++ src/main/java/ni/edu/uam/TestNumericoSumaMultiplicacion/modelo/TestNumerico.java
@@ -0,0 +1,62 @@
+package ni.edu.uam.TestNumericoSumaMultiplicacion.modelo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import org.openxava.annotations.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TestNumerico {
+
+    private static final int TIEMPO_LIMITE_MINUTOS = 4;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idTestNumerico;
+
+    @Required
+    @Column(length = 100)
+    private String nombre;
+
+    @Column(length = 255)
+    private String descripcion;
+
+    private boolean activo;
+
+    @OneToMany
+    @ListProperties("numeroPregunta, operacion")
+    private List<Pregunta> preguntas = new ArrayList<>();
+
+    public List<Pregunta> obtenerPreguntasActivas() {
+        return preguntas.stream()
+                .filter(Pregunta::isActiva)
+                .toList();
+    }
+
+    public boolean validarDisponibilidad() {
+        return activo;
+    }
+
+    public IntentoTest iniciarTest(Usuario usuario) {
+        return new IntentoTest();
+    }
+
+    public void agregarPregunta(Pregunta pregunta) {
+        preguntas.add(pregunta);
+    }
+
+    public void desactivarTest() {
+        this.activo = false;
+    }
+}
